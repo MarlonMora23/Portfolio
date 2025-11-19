3f7552f9967f2b910d2a41db08f5fd3b90d97b9f
Marlon <mdmorar@correo.usbcali.edu.co>
2025-11-19 16:43:14 -0500

    added 2 new projects for project sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +17,5 @@ export default defineConfig({
     }
   },
   output: "server",
-  adapter: vercel()
+  // adapter: vercel()
 });
