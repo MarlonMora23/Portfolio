d664f4700ca6334a8e2db3960dd0c11d6d1f63f8
Marlon <mdmorar@correo.usbcali.edu.co>
2026-03-26 13:47:21 -0500

    Fixed SEO problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import icon from 'astro-icon';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon({
-      include: {
-        "logos": ["*"],
-        "simple-icons": ["*"],
-        "mdi": ["*"]
-      }
-    })
-  ],
+  integrations: [tailwind(), icon({
+    include: {
+      "logos": ["*"],
+      "simple-icons": ["*"],
+      "mdi": ["*"]
+    }
+  }), sitemap()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
